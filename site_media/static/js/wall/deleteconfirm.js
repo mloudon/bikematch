0434b6517f25e8448971fcf5a519ebea287108e7
mloudon <devnull@localhost>
2012-02-18 14:52:45 -0800

    ajax wall post deletion

---
+++ site_media/static/js/wall/deleteconfirm.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+	$('a.delete-confirm-required').each( function(index) {
+		submit_url = $(this).attr('href');
+		confirm_html = '<div class="delete-confirm">'
+    		+ '<p> are you sure? <a href="'+ submit_url +'" class="delete-confirm-option">yes</a> /'
+    		+ ' <a href="" class="delete-confirm-option">no</a></div>'
+		$(this).after(confirm_html);
+		
+		$(this).click(function() {
+       		parentdiv = $(this).parent('div');
+			$('div.delete-confirm',parentdiv).show();
+			return false;
+    	});
+    	
+    	return false;
+	});
+	$('div.delete-confirm').hide();
+});
+
+$("div.delete-confirm").delegate("a.delete-confirm-option", "click", function() {
+	
+	confirmdiv = $(this).parents("div.delete-confirm");
+	
+	linktext = $(this).text();
+	
+	if (linktext == 'no') {
+		
+		confirmdiv.hide();
+		return false;
+	}
+	
+	submit_url = $(this).attr('href');
+	
+	
+	$.post(submit_url,{}, function(data)
+	{
+		if (data.success) {
+				wallitemdiv.remove();
+				
+		} else {
+		    
+		}					
+
+	}, "json");
+	return false;
+	});
