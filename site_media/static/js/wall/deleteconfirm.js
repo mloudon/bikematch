84d84a4a5d78ae7ee4542a88946caa8d296cabb1
mloudon <devnull@localhost>
2012-02-18 19:36:15 -0800

    wall comment deletion and deletion confirm

--- site_media/static/js/wall/deleteconfirm.js
+++ site_media/static/js/wall/deleteconfirm.js
@@ -1,47 +1,46 @@
 function addDelegates()
 {
-	$('div.delete-confirm').delegate('a.delete-confirm-option', 'click', function() {
-		
+	$('div.delete-confirm').delegate('a.delete-confirm-option', 'click', function(event) {
+		event.preventDefault();
 		confirmdiv = $(this).parents('div.delete-confirm');
 		deletediv = $(this).closest('div.deleteable');
 		
 		linktext = $(this).text();
 		if (linktext == 'no') {
 			confirmdiv.hide();
-			return false;
 		}
-		
-		submit_url = $(this).attr('href');
-		$.post(submit_url,{}, function(data)
-		{
-			if (data.success) {
-					deletediv.remove();
-					
-			} else {
-			  //add something here  
-			}					
-	
-		}, 'json');
-		return false;
+		else {
+			submit_url = $(this).attr('href');
+				$.post(submit_url,{}, function(data)
+				{
+					if (data.success) {
+							deletediv.remove();
+							
+					} else {
+					  //add something here  
+					}					
+			
+				}, 'json');
+		}
 	});
 }
 
 $(document).ready(function() {
 
 	$('a.delete-confirm-required').each( function(index) {
+
 		submit_url = $(this).attr('href');
 		confirm_html = '<div class="delete-confirm">'
     		+ '<p> are you sure? <a href="'+ submit_url +'" class="delete-confirm-option">yes</a> /'
     		+ ' <a href="" class="delete-confirm-option">no</a></div>'
 		$(this).after(confirm_html);
 		
-		$(this).click(function() {
+		$(this).click(function(event) {
+       		event.preventDefault();
        		parentdiv = $(this).parent('div');
 			$('div.delete-confirm',parentdiv).show();
-			return false;
     	});
     	
-    	return false;
 	});
 	
 	addDelegates();
